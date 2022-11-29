9164945b60ee32d39eb83437446899835f07dbc2
Ievgenii Makhonko <yobaua@gmail.com>
2022-11-29 02:35:37 -0500

    commit

--- src/main/java/com/studentmanagementsystem/controller/StudentController.java
+++ src/main/java/com/studentmanagementsystem/controller/StudentController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -26,14 +27,38 @@ public class StudentController {
 
     @GetMapping("/students/new")
     public String createStudentForm(Model model) {
-    //create student obj to hold student data
+        //create student obj to hold student data
         Student student = new Student();
         model.addAttribute("student", student);
         return "create_student";
     }
+
     @PostMapping("/students")
-    public String saveStudent(@ModelAttribute("student") Student student){
+    public String saveStudent(@ModelAttribute("student") Student student) {
         studentService.saveStudent(student);
         return "redirect:/students";
     }
+
+    @GetMapping("/students/edit/{id}")
+    public String editStudentForm(@PathVariable Long id, Model model) {
+        model.addAttribute("student", studentService.getStudentById(id));
+        return "edit_student";
+    }
+    @PostMapping("/students/{id}")
+public String updateStudent(@PathVariable Long id,
+                            @ModelAttribute("student") Student student,
+                            Model model){
+        //get Student from database by id
+        Student existingStudent = studentService.getStudentById(id);
+        existingStudent.setId(id);
+        existingStudent.setLastName(student.getLastName());
+        existingStudent.setFirstName((student.getFirstName()));
+        existingStudent.setEmail((student.getEmail()));
+
+
+        //save updated student obj
+        studentService.updateStudent(existingStudent);
+        return "redirect:/students";
+    }
+
 }
